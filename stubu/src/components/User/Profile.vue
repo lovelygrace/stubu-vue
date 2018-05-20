58ffa18bae5ba5977b88a0291f7e040f89b8eb37
Brandon Flores <blflores1@up.edu.ph>
2018-05-20 12:15:33 +0800

    Added Profile

--- stubu/src/components/User/Profile.vue
+++ stubu/src/components/User/Profile.vue
@@ -4,18 +4,18 @@
       <v-flex xs12>
         <v-card color="deep-orange lighten-3">
           <v-card-title>
-            <h4>{{ user.firstname}} {{user.lastname}}</h4>
+            <h4 class="head">{{ user.firstname}} {{user.lastname}}</h4>
           </v-card-title>
         </v-card>
         <v-card color="grey lighten-4"> 
           <v-card-text>
             <div>
               <p>{{ user.description }}</p>
-              <v-text-label v-if="user.specialization !== null">Specialization: </v-text-label><p>{{ user.specialization}}</p>
-              <v-text-label v-if="user.contactnumber !== ''">Contact Number: </v-text-label><p>{{user.contactnumber}}</p>
-              <v-text-label v-if="user.email !== ''">Email:          </v-text-label><p>{{user.email}}</p>
-              <v-text-label v-if="user.degree !== ''">Email:          </v-text-label><p>{{user.degree}}</p>
-              <v-text-label v-if="user.about !== ''">Email:          </v-text-label><p>{{user.aboutme}}</p>
+              <h4 class="p">Contact Number: {{user.contactnumber}}</h4>
+              <h4 class="p">Email: {{user.email}}</h4>
+              <h4 class="p" v-if="isTutor">About Me: {{user.aboutme}}</h4>
+              <h4 class="p" v-if="isTutor">Degree: {{user.degree}}</h4>
+              <h4 class="p" v-if="isTutor">Specialization: {{user.subject}}</h4>
             </div>
           </v-card-text>
         </v-card>
@@ -37,6 +37,13 @@ export default {
     }
   },
   computed: {
+    isTutor () {
+      if (this.tutorIsAuthenticated && this.userIsAuthenticated) {
+        return true 
+      }else {
+        return false
+      }
+    },
     user () {
       if (this.tutorIsAuthenticated && this.userIsAuthenticated) {
         return this.myInfoTutor() 
@@ -65,3 +72,36 @@ export default {
   }
 }
 </script>
+
+<style>
+  .firstcard {
+      margin-bottom: 15px;
+      background-color: #FFAB91;
+      padding-bottom: 20px;
+  }
+  .head {
+    padding-top: 20px;
+    padding-left: 2px;
+    padding-right: 2px;
+    margin-bottom: 5px;
+    color:#424242;
+    font-size: 35px;
+  }
+  .pbigger {
+    font-size: 25px;
+    color:#424242;
+    padding: 0px;
+    margin: 0px;
+  }
+  .p {
+    font-size: 20px;
+    color:#424242;
+    padding: 0px;
+    margin: 2px;
+  }
+  .para {
+      margin: 20px;
+      margin-right: 50px;
+      padding: 10px;
+  }
+</style>
